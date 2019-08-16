ff02d4a03ab4463b37aca5ca3de6dd4dc79b0f68
Gustavo Rodrigues <43577189+kust44@users.noreply.github.com>
2019-08-16 13:55:47 -0300

    Alterações nas classe Model, criação do DAO para mais classes
    Nem tudo pode estar funcionando, não testei com o banco de dados, apenas adaptei o projeto do professor.
    
    Mais comentários nas classes modificadas "author: @Gustavo Rodrigues"

---
+++ src/model/Conexao.java
@@ -0,0 +1,33 @@
+/**
+ * Essa classe eu apenas copiei do projeto de Rafael com BD.
+ * Certamente vai precisar de modificações.
+ */
+
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class Conexao {
+    
+    private static Connection conexao;  
+    
+    public static Connection getConexao(){
+       
+            if(conexao == null){
+                try {
+                    conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_aula","root","");
+                } catch(SQLException e) {
+                    JOptionPane.showMessageDialog(null, "Erro de Conexão!");
+                    throw new RuntimeException(e);
+                } 
+            }
+            return conexao;
+        
+    }
+    
+}
---
+++ src/model/CulturaDao.java
@@ -0,0 +1,191 @@
+package model;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+        
+/**
+ *
+ * @author Gustavo Rodrigues
+ */
+public class CulturaDao {
+    private final Connection conexao;    
+
+    public CulturaDao(Connection conexao) {
+        this.conexao = conexao;
+    }
+    
+    public void inserir(Cultura cultura){
+
+        if(!buscar(cultura)){
+            String sql = "insert into TbCultura (codCultura,nomeCultura,variedade,cicloEmDias,diasGerminacao,diasBercario,diasEngorda) values (?,?,?,?,?,?,?)";
+        
+            PreparedStatement pst;
+            try {
+                pst = conexao.prepareStatement(sql);
+                pst.setInt(1, cultura.getCodCultura());
+                pst.setString(2, cultura.getNomeCultura());
+                pst.setString(3, cultura.getVariedade());
+                pst.setInt(4, cultura.getCicloEmDias());
+                pst.setInt(5, cultura.getDiasGerminacao());
+                pst.setInt(6, cultura.getDiasBercario());
+                pst.setInt(7, cultura.getDiasEngorda());
+                pst.execute();
+                pst.close();
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Não foi possivel inserir.");
+                System.out.println(ex);
+            } 
+            JOptionPane.showMessageDialog(null, "Registro inserido com sucesso.");
+        }
+        else{
+            JOptionPane.showMessageDialog(null, "Não foi possivel inserir. Cultura já cadastrada.");            
+        } 
+        
+    }
+    
+    public boolean excluir(Cultura cultura) {
+        
+        String sql = "delete from TbCultura where codCultura = ?";
+        boolean result = false;
+                
+        if(!buscar(cultura)){
+            try {
+                PreparedStatement pst = conexao.prepareStatement(sql);
+                pst.setInt(1, cultura.getCodCultura());
+                result = pst.execute();
+                pst.close();                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Não foi possivel excluir.");
+                System.out.println(ex);
+            }
+            
+        } else{
+            JOptionPane.showMessageDialog(null, "Cultura não cadastrada.");
+        }
+        
+        return result;     
+        
+    }
+    
+    public void alterar(Cultura cultura) {
+        String sql = "update TbCultura set nomeCultura = ? , variedade = ?, cicloEmDias = ?, diasGerminacao = ?, diasBercario = ?, diasEngorda = ? where codCultura = ?";
+
+        if(buscar(cultura)){
+            try {
+                PreparedStatement pst = conexao.prepareStatement(sql);
+                pst.setString(1, cultura.getNomeCultura());
+                pst.setString(2, cultura.getVariedade());
+                pst.setInt(3, cultura.cicloEmDias);
+                pst.setInt(4, cultura.diasGerminacao);
+                pst.setInt(5, cultura.diasBercario);
+                pst.setInt(6, cultura.diasEngorda);
+                pst.execute();
+                pst.close();                
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Não foi possivel alterar.");
+                System.out.println(ex);
+            }
+            
+        } else{
+            JOptionPane.showMessageDialog(null, "Cultura não cadastrada.");
+        } 
+        
+    }
+    
+    private boolean buscar(Cultura cultura) {
+      
+        String sql = "select * from TbCultura where codCultura like ?";
+        Cultura culturaBusca = new Cultura();
+
+        PreparedStatement pst;
+        ResultSet rs;
+        
+        try {    
+            pst = conexao.prepareStatement(sql);
+            pst.setInt(1,cultura.getCodCultura());
+            rs = pst.executeQuery();
+            while(rs.next()){      
+                culturaBusca.setCodCultura(rs.getInt("codCultura"));
+                culturaBusca.setNomeCultura(rs.getString("nomeCultura"));
+                culturaBusca.setVariedade(rs.getString("variedade"));
+                culturaBusca.setCicloEmDias(rs.getInt("cicloEmDias"));
+                culturaBusca.setDiasGerminacao(rs.getInt("diasGerminacao"));
+                culturaBusca.setDiasBercario(rs.getInt("diasBercario"));
+                culturaBusca.setDiasEngorda(rs.getInt("diasEngorda"));
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel buscar.");
+            System.out.println(ex);
+        }
+        
+        return(culturaBusca.getCodCultura()>0);
+    }
+    
+    public Cultura pesquisar(int codCultura){
+        String sql = "select * from TbCultura where codCultura = ?";
+        Cultura cultura = new Cultura();
+
+        PreparedStatement pst;
+        ResultSet rs;
+        
+        try {    
+            pst = conexao.prepareStatement(sql);
+            pst.setInt(1, codCultura);
+            rs = pst.executeQuery();
+            while(rs.next()){      
+                cultura.setCodCultura(rs.getInt("codCultura"));
+                cultura.setNomeCultura(rs.getString("nomeCultura"));
+                cultura.setVariedade(rs.getString("variedade"));
+                cultura.setCicloEmDias(rs.getInt("cicloEmDias"));                
+                cultura.setDiasGerminacao(rs.getInt("diasGerminacao"));
+                cultura.setDiasBercario(rs.getInt("diasBercario"));
+                cultura.setDiasEngorda(rs.getInt("diasEngorda"));
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel buscar.");
+            System.out.println(ex);
+        }
+        
+        return cultura;
+    }
+    
+    public ArrayList<Cultura> listar() {
+        
+        String sql = "select * from TbCultura order by codCultura";
+        
+        ArrayList<Cultura> lista = new ArrayList<>();
+        
+        PreparedStatement pst;
+        ResultSet rs;
+        try {
+            pst = conexao.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while(rs.next()){
+                Cultura cultura = new Cultura();
+                cultura.setCodCultura(rs.getInt("codCultura"));
+                cultura.setNomeCultura(rs.getString("nomeCultura"));
+                cultura.setVariedade(rs.getString("variedade"));
+                cultura.setCicloEmDias(rs.getInt("cicloEmDias"));
+                cultura.setDiasGerminacao(rs.getInt("diasGerminacao"));
+                cultura.setDiasBercario(rs.getInt("diasBercario"));
+                cultura.setDiasEngorda(rs.getInt("diasEngorda"));
+                lista.add(cultura);
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Não foi possivel listar.");
+            System.out.println(ex);
+        }
+        
+        return lista;    
+        
+    }
+}
