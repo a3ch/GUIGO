51795cc1ff9efb4e14f41827592853457809c4a5
a3ch <43ch@protonmail.ch>
2019-08-17 13:34:28 -0300

    Add Script SQL do banco de dados que foi usado até agora

---
+++ GUIGO.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 17, 2019 at 06:30 PM
+-- Server version: 10.4.6-MariaDB
+-- PHP Version: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `GUIGO`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TbCulturas`
+--
+
+CREATE TABLE `TbCulturas` (
+  `codCultura` int(11) NOT NULL,
+  `nomeCultura` varchar(15) NOT NULL,
+  `variedade` varchar(15) NOT NULL,
+  `cicloEmDias` int(4) NOT NULL,
+  `diasGerminacao` int(11) NOT NULL,
+  `diasBercario` int(11) NOT NULL,
+  `diasEngorda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `TbCulturas`
+--
+
+INSERT INTO `TbCulturas` (`codCultura`, `nomeCultura`, `variedade`, `cicloEmDias`, `diasGerminacao`, `diasBercario`, `diasEngorda`) VALUES
+(1, '1', '1', 1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TbDataRealTime`
+--
+
+CREATE TABLE `TbDataRealTime` (
+  `dataRT` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `temperatura` float NOT NULL,
+  `UMIDADE` float NOT NULL,
+  `LUMINOSIDADE` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `TbUsuario`
+--
+
+CREATE TABLE `TbUsuario` (
+  `codUsuario` int(11) NOT NULL,
+  `nomeUsuario` varchar(45) NOT NULL,
+  `foneUsuario` varchar(15) NOT NULL,
+  `emailUsuario` varchar(100) NOT NULL,
+  `loginUsuario` varchar(15) NOT NULL,
+  `senhaUsuario` varchar(15) NOT NULL,
+  `tipoUsuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `TbUsuario`
+--
+
+INSERT INTO `TbUsuario` (`codUsuario`, `nomeUsuario`, `foneUsuario`, `emailUsuario`, `loginUsuario`, `senhaUsuario`, `tipoUsuario`) VALUES
+(1, 'banana', '9999999', 'dasdasdasdasd', 'banana', '1234', 3),
+(4, 'Alexandre', '99-9 9999 9999', 'jsbahsbdh', 'bessa', '123', 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `TbCulturas`
+--
+ALTER TABLE `TbCulturas`
+  ADD PRIMARY KEY (`codCultura`);
+
+--
+-- Indexes for table `TbDataRealTime`
+--
+ALTER TABLE `TbDataRealTime`
+  ADD PRIMARY KEY (`dataRT`);
+
+--
+-- Indexes for table `TbUsuario`
+--
+ALTER TABLE `TbUsuario`
+  ADD PRIMARY KEY (`codUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `TbCulturas`
+--
+ALTER TABLE `TbCulturas`
+  MODIFY `codCultura` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `TbUsuario`
+--
+ALTER TABLE `TbUsuario`
+  MODIFY `codUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
